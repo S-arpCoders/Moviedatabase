29fd85bcbfa837026a907c02d8822ef135a6dea4
clement maimane <tshwaraganangmaimane25@gmail.com>
2024-12-02 11:39:18 +0200

    Project setup /installation of packages

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import { useState } from 'react';
+
+
+const Getapi =()=>{
+
+  const [apifetch,setApifetch]= useState("");
+
+
+}
+
+
+
+function App() {
+  return (
+    <div className="App">
+  <h1>welcome our movie app</h1>
+    </div>
+  );
+}
+
+export default App;
