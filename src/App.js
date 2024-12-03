5e3f761a0f8459c9c724dded2afed3f131f51c4d
Hlanganani <127399757+Msawekho@users.noreply.github.com>
2024-12-03 11:38:27 +0200

    Re-directed to run the correct Movie details.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MovieDetails from "./pages/MovieDetails";
+import MovieDetails from "./components/MovieDetails/MovieDetails";
 import Homepage from "./pages/homepage";
 
 const App = () => {
